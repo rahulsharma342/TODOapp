ee22b7e331592fe74a3b01f8873c683b759b0e58
rahulsharma342 <rahulkv247@gmail.com>
2025-11-18 14:43:49 +0530

    New responsive project

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn1=document.querySelector(".adbtn");
+const input=document.querySelector("input");
+
+btn1.addEventListener("click",()=>{
+    const li=document.createElement("li");
+    const span=document.createElement("span");
+    span.textContent=input.value;
+    li.appendChild(span);
+    const ul=document.querySelector(".list");
+    ul.appendChild(li);
+    const btn=document.createElement("button");
+    btn.textContent="Delete";
+
+    li.appendChild(btn);
+    btn.addEventListener("click",()=>{
+        ul.removeChild(li);
+    })
+})
